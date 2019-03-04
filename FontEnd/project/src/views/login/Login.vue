6930d104145085b7bf8ea180d8a7c5c56a3fd826
Tang233 <1825949538@qq.com>
2019-03-04 20:42:43 +0800

    login&register

---
+++ FontEnd/project/src/views/login/Login.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="login">
+    <h3>登录</h3>
+      <div ><input class="inputUsername" type="text" name="username" placeholder="请输入用户名"></div>
+      <div ><input class="inputPassword" type="text" name="password" placeholder="请输入密码"></div>
+      <div ><button class="loginButton" type="button" name="loginButton">登录</button></div>
+      <span ">没有账号？马上注册</span>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+.login{
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.inputUsername{
+  width: 200px;
+  height: 25px;
+  margin-top: 15px;
+}
+.inputPassword{
+  width: 200px;
+  height: 25px;
+  margin-top: 15px;
+}
+.loginButton{
+  width: 205px;
+  height: 25px;
+  margin-top: 15px;
+}
+</style>
