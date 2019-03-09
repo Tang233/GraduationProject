22d966c365fe7a74c1aa6a4b591e08c6a7dc1b41
Tang233 <1825949538@qq.com>
2019-03-09 15:24:19 +0800

    登录跨域请求

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,11 +2,16 @@ const express = require('express')//引入express模块
 const mysql = require('mysql');     //引入mysql模块
 const app = express()//把对象赋给app
 var bodyParser=require('body-parser');//对post请求进行解析
+var cookieParser = require('cookie-parser');
 var https = require('https');
 var qs = require('querystring');
+var cors = require('cors');
 
+
+var cookieConfigure={maxAge:3000,path:'/',httpOnly:false};
 app.use(bodyParser.json());
-
+// app.use(cookieParser())
+app.use(cors({origin:true, credentials:true}));
 // app.get('/', (req, res) => res.send('Root'))
 var connection = mysql.createConnection({      //创建mysql实例
     host:'127.0.0.1',
@@ -16,17 +21,17 @@ var connection = mysql.createConnection({      //创建mysql实例
     database:'expressdb'
 });
 connection.connect();
-// var sql = 'SELECT * FROM user';
-// var str = " ";
-// connection.query(sql, function (err,result) {
-//     if(err){
-//         console.log('[SELECT ERROR]:',err.message);
-//     }
-//     str = JSON.stringify(result);
-//     //数据库查询的数据保存在result中，但浏览器并不能直接读取result中的结果，因此需要用JSON进行解析
-//     //console.log(result);   //数据库查询结果返回到result中
-//     console.log(str);
-// });
+var sql = 'SELECT * FROM user';
+var str = " ";
+connection.query(sql, function (err,result) {
+    if(err){
+        console.log('[SELECT ERROR]:',err.message);
+    }
+    str = JSON.stringify(result);
+    //数据库查询的数据保存在result中，但浏览器并不能直接读取result中的结果，因此需要用JSON进行解析
+    //console.log(result);   //数据库查询结果返回到result中
+    console.log(str);
+});
 app.get('/',function (req,res) {
   console.log(123);
   var str="123"
@@ -43,19 +48,21 @@ app.post('/login', function (req,res) {
   console.log("login:");
   // res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Origin', req.header('Origin'));
-	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Headers', 'content-type,Authorization')
-	res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-	res.header( "Access-Control-Max-Age", "1000" ); //
-	res.header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+res.header('Access-Control-Allow-Credentials', true);
+res.header('Access-Control-Allow-Headers', 'content-type,Authorization')
+res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+res.header( "Access-Control-Max-Age", "1000" ); //
+res.header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
 
-  var sqlString='select * from user where user_id = ?;';
 
-  console.log(req.body.user_id);
+  var sqlString="select * from user where user_id = ?;";
+
 	// req.body=JSON.parse(req.body);
 	var user_id=req.body.user_id;
 	var user_pwd=req.body.user_pwd;
-	mysql.query(sqlString,[user_id],function(results){
+	connection.query(sqlString,[user_id],function(err,results){
+    console.log(user_id);
+    console.log(results);
 		if(results.length){
 			if(user_pwd==results[0].user_pwd){
 				res.cookie('user','user_id='+user_id,cookieConfigure);
