d146d453d578ca101d4668317ab8e9858c62575a
Tang <351211168@qq.com>
2019-04-12 15:04:05 +1000

    for pull

--- FontEnd/project/src/App.vue
+++ FontEnd/project/src/App.vue
@@ -11,10 +11,19 @@ export default {
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+}
 html,body{
   height: 100%;
+  width: 100%;
+  top: 0px;
+  left: 0px;
 }
 #app {
+  height: 100%;
+  width: 100%;
   padding: 0px;
   margin: 0px;
   position: absolute;
