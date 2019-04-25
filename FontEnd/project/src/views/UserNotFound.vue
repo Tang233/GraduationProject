bd189ea3731b83279683ec5263f3d1cfdcf7143a
Tang233 <1825949538@qq.com>
2019-04-26 03:57:52 +0800

    applyadoption

--- FontEnd/project/src/views/UserNotFound.vue
+++ FontEnd/project/src/views/UserNotFound.vue
@@ -1,6 +1,6 @@
 <template lang="html">
 <div class="notfound-box">
-  404
+
 </div>
 </template>
 
@@ -10,4 +10,11 @@ export default {
 </script>
 
 <style lang="css">
+body{
+  background-image: url("../../images/404.jpg");
+  background-size: 500px 473px;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
 </style>
